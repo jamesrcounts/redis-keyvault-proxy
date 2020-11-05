8f9e1c97d65dc4b2da57cb20df6e92bebc72b1e3
Jim Counts <jamesrcounts@outlook.com>
2020-11-05 05:39:57 +0000

    Update Redis name

--- infrastructure/azure/scripts/rotate-primary-redis-acess-key.sh
+++ infrastructure/azure/scripts/rotate-primary-redis-acess-key.sh
@@ -2,5 +2,5 @@
 set -euox pipefail
 
 az redis regenerate-keys --key-type Primary \
-                         --name rc-meet-buzzard \
+                         --name rc-sharp-satyr \
                          --resource-group rg-redis-keyvault-proxy
