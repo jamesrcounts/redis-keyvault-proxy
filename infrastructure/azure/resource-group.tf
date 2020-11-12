118585c1f3caf2a60a67cb8f2b330e52ee583a70
Jim Counts <jamesrcounts@outlook.com>
2020-11-12 07:36:37 -0800

    Remove extra role assignment

--- infrastructure/azure/resource-group.tf
+++ infrastructure/azure/resource-group.tf
@@ -4,14 +4,8 @@ resource "azurerm_resource_group" "primary" {
   tags     = local.tags
 }
 
-resource "azurerm_role_assignment" "contact_manager" {
+resource "azurerm_role_assignment" "keyvault_deployment_administrator" {
   principal_id         = data.azurerm_client_config.current.object_id
-  role_definition_name = "Key Vault Administrator (preview)"
-  scope                = azurerm_resource_group.primary.id
-}
-
-resource "azurerm_role_assignment" "key_vault_administrator" {
-  principal_id         = "b55f6ee9-3a4b-42bf-ad67-d02c4632a010"
   role_definition_name = "Key Vault Administrator (preview)"
   scope                = azurerm_resource_group.primary.id
 }
