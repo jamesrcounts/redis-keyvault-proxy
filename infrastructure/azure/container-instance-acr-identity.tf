ecb32242161256ee1c621401139f77af470cce29
Jim Counts <jamesrcounts@outlook.com>
2021-02-19 00:40:56 +0000

    Use application password.

--- infrastructure/azure/container-instance-acr-identity.tf
+++ infrastructure/azure/container-instance-acr-identity.tf
@@ -10,11 +10,11 @@ resource "azuread_service_principal" "cache_worker_principal" {
   application_id = azuread_application.cache_worker.application_id
 }
 
-resource "azuread_service_principal_password" "cache_worker_principal_password" {
-  service_principal_id = azuread_service_principal.cache_worker_principal.id
-  description          = "cache-worker password"
-  value                = random_password.cache_worker_password.result
-  end_date_relative    = "240h"
+resource "azuread_application_password" "cache_worker_principal_password" {
+  application_object_id = azuread_application.cache_worker.id
+  description           = "cache-worker password"
+  value                 = random_password.cache_worker_password.result
+  end_date_relative     = "240h"
 }
 
 resource "random_password" "cache_worker_password" {
