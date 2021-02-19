42763df06af0cd8d260d35fd3ab2dbeba9a9a8f4
Jim Counts <jamesrcounts@outlook.com>
2021-02-19 00:40:42 +0000

    Update providers

--- infrastructure/azure/keyvault-ops.tf
+++ infrastructure/azure/keyvault-ops.tf
@@ -7,7 +7,6 @@ resource "azurerm_key_vault" "ops" {
   name                            = replace("kv-ops-${local.project}", "-", "")
   resource_group_name             = azurerm_resource_group.primary.name
   sku_name                        = "standard"
-  soft_delete_enabled             = true
   soft_delete_retention_days      = 30
   tenant_id                       = data.azurerm_client_config.current.tenant_id
   tags                            = local.tags
